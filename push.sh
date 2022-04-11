35685afbc92e05c0e17ccabf20207418788493a6
Juan Montesinos <juanfelipe.montesinos@upf.edu>
2022-04-11 17:12:41 +0200

    Added some extra dependencies

---
+++ push.sh
@@ -0,0 +1,2 @@
+sudo docker push jfmontgar/av-pytorch:latest
+sudo docker push jfmontgar/av-pytorch:1.10.0-cuda11.3-runtime
--- test.py
+++ test.py
@@ -4,3 +4,6 @@ import torchvision
 import numpy
 import librosa
 import matplotlib.pyplot as plt
+import pytorch_lightning
+import torch_stoi
+import torch_mir_eval
